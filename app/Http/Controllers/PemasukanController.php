d95468f03965cdbb362eef47f6304dca794cf2ce
agusmuhamadgi <gaostulibad@gmail.com>
2023-03-31 03:02:22 +0700

    menambah pemasukan dan distributor

--- app/Http/Controllers/PemasukanController.php
+++ app/Http/Controllers/PemasukanController.php
@@ -2,13 +2,129 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Http;
 
 class PemasukanController extends Controller
 {
     public function pemasukan()
     {
-        $data['title'] = 'Pemasukan';
+        $data['title'] = 'Kelola Pemasukan';
+
+        $token = session('access_token');
+
+        $response = Http::withToken("$token")->get('http://backendkeuangan.dlhcode.com/api/pemasukan');
+        $body_pemasukan = $response->getBody();
+        $data['pemasukan'] = json_decode($body_pemasukan, true);
+        $data['pemasukan'] = $data['pemasukan']['data'];
         return view('pemasukan', $data);
     }
+
+    public function tambah_pemasukan(Request $request)
+    {
+        $token = session('access_token');
+
+        $addPemasukan = [
+            'jenis_pemasukan' => $request->jenis_pemasukan,
+            'keterangan' => $request->keterangan,
+            'tgl' => $request->tgl,
+            'total_pemasukan' => $request->total_pemasukan,
+            'updated_at' => now(),
+            'created_at' => now(),
+        ];
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token, // token autentikasi
+            'Accept' => 'application/json', // format respon
+        ])->post('http://backendkeuangan.dlhcode.com/api/tambah_pemasukan', $addPemasukan);
+
+       
+
+        if ($response->ok()) {
+            $response->json(); // data response jika request sukses
+            // lakukan sesuatu dengan data response
+            return redirect()
+                ->route('pemasukan')
+                ->withSuccess('Pemasukan berhasil ditambahkan');
+        } else {
+            $errorMessage = $response->serverError() ? 'Server error' : 'Client error'; // pesan error
+            $errorMessage .= ': ' . $response->body(); // tambahkan pesan error dari body response
+            // lakukan sesuatu dengan pesan error
+            return redirect()->route('pemasukan')
+                ->with('error', 'Pemasukan gagal disimpan');
+        }
+    }
+
+    public function update_pemasukan(Request $request, $id)
+    {
+        $token = session('access_token');
+        $client = new Client([
+            'base_uri' => 'http://backendkeuangan.dlhcode.com/api/',
+            'timeout' => 50.0,
+        ]);
+
+        $response = $client->request('PUT', "update_pemasukan/$id", [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Accept' => 'application/x-www-form-urlencoded',
+            ],
+            'json' => [
+                'jenis_pemasukan' => $request->jenis_pemasukan,
+                'keterangan' => $request->keterangan,
+                'tgl' => $request->tgl,
+                'total_pemasukan' => $request->total_pemasukan,
+                'updated_at' => now(),
+            ],
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+        return redirect()
+            ->route('pemasukan')
+            ->withSuccess('Data Pemasukan berhasil diubah');
+    }
+
+    public function edit_pemasukan($id)
+    {
+        $data['title'] = 'Edit Kota';
+        $token = session('access_token');
+        $client = new Client([
+        'base_uri' => 'http://backendkeuangan.dlhcode.com/api/',
+        'timeout' => 2.0,
+        ]);
+    
+        $response = $client->request('GET', "get_pemasukan_by_id/$id", [
+        'headers' => [
+        'Authorization' => 'Bearer ' . $token,
+        'Accept' => 'application/json',
+        ]
+        ]);
+    
+    
+        $data['pemasukan'] = json_decode($response->getBody(), true);
+        $data['pemasukan'] = $data['pemasukan']['data'][0];
+   
+        return view('edit_pemasukan', $data);
+    }
+
+    public function delete_pemasukan($id)
+    {
+        $token = session('access_token');
+        $client = new Client([
+        'base_uri' => 'http://backendkeuangan.dlhcode.com/api/',
+        'timeout' => 2.0,
+        ]);
+
+        $response = $client->request('DELETE', "delete_pemasukan/$id", [
+        'headers' => [
+        'Authorization' => 'Bearer ' . $token,
+        'Accept' => 'application/json',
+        ]
+        ]);
+
+        return redirect()
+            ->route('pemasukan')
+            ->withSuccess('Data Pemasukan berhasil dihapus');
+    }
+
 }
--- resources/views/pemasukan.blade.php
+++ resources/views/pemasukan.blade.php
@@ -22,28 +22,125 @@
     @include('layouts.header')
     <!-- Navbar End -->
     <div class="card-body">
-
         <div class="col-12">
             <div class="bg-light rounded h-100 p-4">
                 <h6 class="mb-4">Data {{ $title }}</h6>
-                
+                 <!-- Table Start -->
+                 @if (Session::has('success'))
+            <div class="alert alert-success">
+                {{ Session::get('success') }}
+            </div>
+            @elseif (Session::has('errors'))
+            <div class="alert alert-danger">
+                {{ Session::get('errors') }}
+            </div>
+            @endif
+ <div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl">
+        <div class="col-12">
+            <div class="card mb-5">
+                <div class="card-body">
+                    <h4 class="card-title">FORM TAMBAH DATA PEMASUKAN</h4>
+                    <form action="{{ route('tambah.pemasukan') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                        <div class="col-lg-6">
+                      <div class="mb-3">
+                        <label for="jenis_pemasukan" class="form-label">Jenis Pemasukan</label>
+                        <input class="form-control" name="jenis_pemasukan" id="jenis_pemasukan">
+                      </div>
+                      <div class="mb-3">
+                          <label for="keterangan" class="form-label">Keterangan</label>
+                          <textarea class="form-control" name="keterangan" id="keterangan"></textarea>
+                      </div>
+                        <div class="mb-3">
+                          <label for="tgl" class="form-label">Tanggal</label>
+                          <input type="date" class="form-control" name="tgl" id="tgl">
+                        </div>
+                        <div class="mb-3">
+                          <label for="total_pemasukan" class="form-label">Total Pemasukan</label>
+                          <input class="form-control" name="total_pemasukan" id="total_pemasukan">
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            Tambah
+                          </button>
+                        </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+           
+            <div class="card shadow">
+                <div class="card-body">
+              <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Data Pemasukan</h6>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Jenis Pemasukan</th>
+                                <th scope="col">Keterangan</th>
+                                <th scope="col">Tanggal</th>
+                                <th scope="col">Total Pemasukan</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($pemasukan as $index => $pm)
+                                    <tr>
+                                        <td>{{ $index+1 }}</td>
+                                        <td>{{ $pm['jenis_pemasukan'] }}</td>
+                                        <td>{{ $pm['keterangan'] }}</td>
+                                        <td>{{ $pm['tgl'] }}</td>
+                                        <td>Rp. {{ number_format($pm['total_pemasukan']) }}</td>
+                                        <td>
+                                            <div class="d-flex flex-wrap gap-2">
+                                                <a href="{{ route('edit.pemasukan', ['id' => $pm['id']]) }}" type="button"
+                                                    class="btn btn-outline-primary waves-effect waves-light">
+                                                    Edit</a>
+                                                   <form action="{{ route('delete.pemasukan', ['id' => $pm['id']]) }}" method="POST">
+                                                    {{ csrf_field() }}
+                                                    {{ method_field('DELETE') }}
+                                                    <button onclick="return confirm('Anda yakin akan menghapus ini? ')" type="submit"
+                                                    class="btn btn-outline-danger waves-effect waves-light">Hapus</i></button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+                </div>
             </div>
         </div>
     </div>
-
+</div>
+<!-- Table End -->
+            </div>
+            
+        </div>
+    </div>
 </div>
 <!-- Content End -->
+<!-- Button trigger modal -->
 
+  
+  
 <!-- Back to Top -->
 <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top">
     <i class="bi bi-arrow-up"></i>
 </a>
 </div>
 
-@endsection
 <!--End wrapper-->
 <script>
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
 </script>
+@endsection
