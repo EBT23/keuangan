91cb54ffb09e4c7769f27d0c25b5c7c1b752585e
agusmuhamadgi <gaostulibad@gmail.com>
2023-03-31 22:38:37 +0700

    perbaiki pemasukan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,10 @@
 <!-- Sidebar Start -->
 <div class="sidebar pe-4 pb-3">
     <nav class="navbar bg-light navbar-light">
-        <a href="index.html" class="navbar-brand mx-4 mb-3">
-            <h3 class="text-primary">
-                <i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+        <a href="#" class="navbar-brand mx-4 mb-3">
+            <h6 class="text-primary">
+                {{-- <img src="{{ asset('img/logo-pvc.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
+        <span class="brand-text font-weight-light">PT. PANORAMA VARIA CIPTA</span></i></h6>
         </a>
         <div class="d-flex align-items-center ms-4 mb-4">
             <div class="position-relative">
